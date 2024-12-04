3f428a62b926e4ac2bc4f0e379f32429065a743d
Ognjen <ognjendimitrijevic006@gmail.com>
2024-12-04 21:20:27 +0100

    Fixed bugged API calls

--- src/main/java/com/jahaci/edukacija/controller/UserController.java
+++ src/main/java/com/jahaci/edukacija/controller/UserController.java
@@ -32,7 +32,7 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Integer id) {
-        userService.deleteUser(id);
+        //userService.deleteUser(id);
     }
 
     @GetMapping("/login")
